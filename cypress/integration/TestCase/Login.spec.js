daf0ec6260f9455714c9104c594d8078422fe971
pranitamehetre <mehetrep2403@gmail.com>
2023-01-20 15:22:04 +0530

    third  commit

--- cypress/integration/TestCase/Login.spec.js
+++ cypress/integration/TestCase/Login.spec.js
@@ -1,10 +1,61 @@
 /// <reference types="cypress" />
 import LoginPage from "../page_object_model/LoginPage";
-before('visit url',function(){
-    LoginPage.loadLoginPage()
+beforeEach('visit url',function(){
+    LoginPage.VisitPage()
 })
+
 describe('verify the login page',function(){
-    it('verify username and password',function(){
+
+    it('TC01 verify URL',function(){
+        LoginPage.loadLoginPage()
+    })
+
+    it('TC02 verify login page with valid username and password',function(){
         LoginPage.checkLogin('pranita','pranita')
+    })
+
+    it('TC03 verify login functionality with space as username and password',function(){
+        LoginPage.checkLogin('  ','  ')
+    })
+
+    it('TC04 verify login functionality with special symbol as input',function(){
+        LoginPage.checkLogin('@#','@$')
+    })
+
+    it('TC05 check login without entering password error msg shown',function(){
+        LoginPage.withoutUserPass()
+    })
+
+    it('TCO6 check login without entering username error msg shown',function(){
+        LoginPage.withoutUsername('pranita')
+    })
+
+    it('TC07 check login without entering password error msg shown',function(){
+        LoginPage.withoutPassword('pranita')
+    })
+
+    it('TC08 check login functionality without click on Remember me button',function(){
+        LoginPage.withoutRememberMe('pranita','pranita')
+    })
+
+    it('TC09 After successfully login check page landed on homepage',function(){
+        LoginPage.checkLogin('pranita','pranita')
+        LoginPage.homePageURL()
+    })
+
+    it('TC10 verify table length',function(){
+        LoginPage.checkLogin('pranita','pranita')
+        LoginPage.checkTable()
+
+    })
+     
+    it('TC11 After login check for AMOUNT header clickable',function(){
+        LoginPage.checkLogin('pranita','pranita')
+        LoginPage.clickAmountHeader()
+    })
+
+    it('TC12 verify element are sorted',function(){
+        LoginPage.checkLogin('pranita','pranita')
+        LoginPage.sortingElement()
     })
 })
--- cypress/integration/page_object_model/LoginPage.js
+++ cypress/integration/page_object_model/LoginPage.js
@@ -1,15 +1,73 @@
 /// <reference types="cypress" />
 class LoginPage{
-    static loadLoginPage(){
+    static VisitPage(){
         cy.visit('https://sakshingp.github.io/assignment/login.html')
-        cy.url().should('include',"loginn.html")
     }
+
+    static loadLoginPage(){
+         cy.url().should('include',"login.html")
+    }
+
     static checkLogin(username,password){
         cy.get('#username').type(username)
         cy.get('#password').type(password)
         cy.get('[type="checkbox"]').check()
         cy.get('#log-in').click()
         cy.get('.logo-label').should('contain',"DEMO")
+    }
+
+    static withoutUserPass(){
+        cy.get('#log-in').click()
+        cy.wait(4000)
+        cy.get('div[role="alert"]').last().should('contain',"Both Username and Password")
+    }
+
+    static withoutUsername(password){
+        cy.get('#password').type(password)
+        cy.get('[type="checkbox"]').check()
+        cy.get('#log-in').click()
+        cy.get('div[role="alert"]').last().should('contain',"Username must be present")
+    }
+
+    static withoutPassword(username){
+        cy.get('#username').type(username)
+        cy.get('[type="checkbox"]').check()
+        cy.get('#log-in').click()
+        cy.get('div[role="alert"]').last().should('contain',"Password must be present")
+    }
+
+    static withoutRememberMe(username,password){
+        cy.get('#username').type(username)
+        cy.get('#password').type(password)
+        cy.get('#log-in').click()
+        cy.get('.logo-label').should('contain',"DEMO")
+    }
+
+    static homePageURL(){
+        cy.url().should('include',"home.html")
+    }
+
+    static checkTable(){
+        cy.get('#transactionsTable tbody tr ').should('have.length',6)
+    }
+
+    static clickAmountHeader(){
+        cy.get('#amount').click().should('have.id',"amount")
+    }
+
+    static sortingElement(){
+        cy.get('table>thead>tr>th').last().should('be.visible').click()
+        let arr = []
+        
+        cy.get('table>tbody>tr').each(function (el) {
+
+            let element = (el).find('td').last().text()
+            let ele = (element.replace('USD', ' ').trim())
+            arr.push(parseFloat(ele.split(' ').join('').replace(',', '')))
+        }).then(function () {
+            cy.log(arr)
+            expect(arr).to.eq(arr.sort())
+        })
     }
     
 }
